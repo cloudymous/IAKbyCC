164177c379ee9dda48a6e11f5db44dd3b5d249e5
cloudymous <sayacloud@gmail.com>
2017-11-18 11:52:47 +0800

    Initial commit

---
+++ app/src/main/java/com/visitgowa/basketstore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.visitgowa.basketstore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
